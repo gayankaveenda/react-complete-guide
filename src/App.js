3c93d9f10bbcc304caee6124ee808e00698116f4
Gayan Kaveenda <gayankaveenda@gmail.com>
2023-04-12 23:36:23 +1000

    remove repetitions and add further styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ExpenseItem from "./components/ExpenseItem";
 import ExpenseAsList from "./components/resources/ExpenseList";
+import "./components/resources/ExpenseList";
 
 function App() {
   const expenses = [
@@ -25,28 +26,8 @@ function App() {
   ];
 
   return (
-    <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-      <ExpenseAsList expenseList={expenses}></ExpenseAsList>
+    <div className='expenses'>
+      <ExpenseAsList expenseList={expenses} />
     </div>
   );
 }
--- src/components/resources/ExpenseList.js
+++ src/components/resources/ExpenseList.js
@@ -1,14 +1,17 @@
 import ExpenseItem from "../../components/ExpenseItem";
+import "./ExpenseList.css";
 
 const ExpenseAsList = (props) => {
   const Expenses = props.expenseList;
 
   const listItems = Expenses.map((element, index) => (
-    <ExpenseItem key={element.id}
-      title={element.title}
-      amount={element.amount}
-      date={element.date}
-    ></ExpenseItem>
+    <div key={index.toString()} id={element.title + index.toString()}>
+      <ExpenseItem
+        title={element.title}
+        amount={element.amount}
+        date={element.date}
+      ></ExpenseItem>
+    </div>
   ));
   return listItems;
 };
